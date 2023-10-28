f1863481de63d7bee8779692b59a67b1b49a104c
Bartosz Woldanski <studio@dziwnykot.pl>
2023-10-28 22:51:30 +0200

    feat: add Query model and prepare migration

---
+++ database/migrations/2023_09_24_202438_add_to_queries_api_name_column.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+ /**
+  * Run the migrations.
+  */
+ public function up(): void
+ {
+  Schema::table('queries', function (Blueprint $table) {
+   $table->string('api-name', 30);
+  });
+
+ }
+
+ /**
+  * Reverse the migrations.
+  */
+ public function down(): void
+ {
+  Schema::table('queries', function (Blueprint $table) {
+   //
+   $table->dropIfExists('api-name');
+  });
+ }
+};
