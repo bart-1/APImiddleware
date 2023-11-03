4233bbd4f6f8e056a3ec7b409cd370f6c2c4db1a
Bartosz Woldanski <studio@dziwnykot.pl>
2023-11-03 12:11:52 +0100

    refactor: set "apiUrl" param as recognition value
    Change param "apiName" to param "apiUrl" as dominant value for
    recognition of DB record.

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -13,25 +13,22 @@ class APIController extends Controller
 
  protected $repository;
  public $apiResponseData;
- protected $url;
+ protected $apiUrl;
  protected $apiName;
 
  public function __construct(APIQueryRepository $repository)
  {
   $this->repository = $repository;
-
  }
 
- public function testAPIConnection(string $url): bool
+ public function testApiUrl(string $apiUrl): bool
  {
-
-  if (filter_var($url, FILTER_VALIDATE_URL)
-   && get_headers($url)) {
+  if (filter_var($apiUrl, FILTER_VALIDATE_URL)
+   && get_headers($apiUrl)) {
    return true;
   } else {
    return false;
   }
-
  }
 
  public function verifyLocalData($localData): void
@@ -42,24 +39,24 @@ class APIController extends Controller
       $this->apiResponseData = $localData->value('api-response');
 
     } else {
-   $this->fetchAPI($this->url);
+   $this->fetchAPI($this->apiUrl);
    $id = $localData->value('id');
    $this->repository->update($id, ["api-response" => $this->apiResponseData, "api-name" => $this->apiName]);
 
   }
-
  }
 
- public function manageQuery(): void
+ public function manageQuery()
  {
-  $localData = $this->repository->findByValue('api-name', $this->apiName);
+  $localData = $this->repository->findByValue('api-url', $this->apiUrl);
+
   switch (true) {
    case $localData !== null:
     $this->verifyLocalData($localData);
     break;
    case $localData === null:
-    $this->fetchAPI($this->url);
-    $this->repository->create(["api-response" => $this->apiResponseData, "api-name" => $this->apiName]);
+    $this->fetchAPI($this->apiUrl);
+    $this->repository->create(["api-response" => $this->apiResponseData, "api-name" => $this->apiName, "api-url"=> $this->apiUrl]);
     break;
 
   }
@@ -67,23 +64,23 @@ class APIController extends Controller
 
  public function postQueryHandler(Request $request)
  {
-
-  if ($request->input('url') && $request->input('apiName')) {
-   $this->url     = $request->input('url');
-   $this->apiName = $request->input('apiName');
-   $this->manageQuery();
-   return $this->apiResponseData;
+if ($request->input('apiUrl') && $request->input('apiName') && $this->testApiUrl($request->input('apiUrl'))) {
+    $this->apiUrl     = $request->input('apiUrl');
+    $this->apiName = $request->input('apiName');
+    $this->manageQuery();
+    return $this->apiResponseData;
+} else if($request->input('apiUrl') && $request->input('apiName') && !$this->testApiUrl($this->apiUrl)) {
+    return response('wrong API URL', 422);
   } else {
    return response('not accepted query body', 422);
   }
  }
 
- public function fetchAPI($url): void
+ public function fetchAPI($apiUrl): void
  {
-
-  if ($this->testAPIConnection($url)) {
-   $conn = \curl_init($url);
-   curl_setopt($conn, CURLOPT_URL, $url);
+  if ($this->testAPIUrl($apiUrl)) {
+   $conn = \curl_init($apiUrl);
+   curl_setopt($conn, CURLOPT_URL, $apiUrl);
    curl_setopt($conn, CURLOPT_HEADER, false);
    curl_setopt($conn, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
 
