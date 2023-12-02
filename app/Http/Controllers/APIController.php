7d03655dcefc9939929a064ae6f32d85caff5f21
Bartosz Woldanski <studio@dziwnykot.pl>
2023-12-02 18:33:16 +0100

    Refactor(fix): new APIController structure
    BREAKING CHANGE: add main method in controller class, which calls
    other helpers methods to validate request elements.
    Validation methods return bool instead of chain calls where
    one method calls another.

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -6,6 +6,7 @@ use App\Repositories\APIQueryRepository;
 use App\Traits\TestApiUrl;
 use App\Traits\TimestampCompare;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class APIController extends Controller
 {
@@ -15,6 +16,7 @@ class APIController extends Controller
 
  protected $repository;
  public $apiResponseData;
+ public $dbResponseData;
  protected $apiUrl;
  protected $apiName;
  public $apiAcceptedDataFreshness;
@@ -25,78 +27,156 @@ class APIController extends Controller
   $this->repository                                                           = $repository;
  }
 
- public function apiKeyVerification(Request $request)
+/**
+ * Controller main method. Verifies request, validates it elements and return response. Manages errors responses.
+ * @param \Illuminate\Http\Request $request
+ * @return Response
+ */
+
+ public function apiController(Request $request)
  {
-  if ($request->input('apiKey') === \env('API_KEY')) {
-   return $this->validatePOSTContent($request);
-  } else {
-   return response('Wrong API key. Access denied.', 403);
-  }
-
- }
-
-  public function validatePOSTContent($request)
- {
-  if ($request->input('apiUrl') && $request->input('apiName') && $this->testApiUrl($request->input('apiUrl'))) {
-   $this->apiUrl                                                                           = $request->input('apiUrl');
-   $this->apiName                                                                          = $request->input('apiName');
-   $request->input('apiAcceptedDataFreshness') !== null && $this->apiAcceptedDataFreshness = $request->input('apiAcceptedDataFreshness');
-   $this->checkLocalData();
-   return $this->apiResponseData;
-  } else if ($request->input('apiUrl') && $request->input('apiName') && !$this->testApiUrl($this->apiUrl)) {
-   return response('wrong API URL', 422);
-  } else {
-   return response('not accepted query body', 422);
-  }
- }
-
-  public function checkLocalData()
- {
-  $localData = $this->repository->findByValue('api-url', $this->apiUrl);
-
-  switch (true) {
-   case $localData !== null:
-    $this->verifyLocalDataFreshness($localData);
-    break;
-   case $localData === null:
+  switch (false) {
+   case $this->apiKeyVerification($request):
+    return response('Wrong API key. Access denied.', 403);
+   case $this->validatePOSTContent($request):
+    return response('not accepted query body', 422);
+   case $this->validateUrlPOSTContent($this->apiUrl):
+    return response('wrong API URL', 422);
+   case $this->checkLocalDataExist('api-url', $this->apiUrl):
     $this->fetchAPI($this->apiUrl);
     $this->repository->create(["api-response" => $this->apiResponseData, "api-name" => $this->apiName, "api-url" => $this->apiUrl]);
     break;
-
+   case !$this->checkLocalDataExist('api-url', $this->apiUrl):
+    $this->loadFindedDataFromDB($this->apiUrl);
+    if (!$this->verifyLocalDataFreshness($this->dbResponseData)) {
+     $this->updateFindedDataInDB($this->apiUrl, $this->dbResponseData);
+    }
+    $this->apiResponseData = $this->dbResponseData->value('api-response');
   }
+
+  return $this->apiResponseData;
  }
 
- public function verifyLocalDataFreshness($localData): void
+/**
+ * Method verifies apiKey value from POST request by comparing it with API_KEY line in .env file.
+ * @param \Illuminate\Http\Request $request
+ * @return bool
+ */
+
+ public function apiKeyVerification($request): bool
  {
+  return ($request->input('apiKey') === \env('API_KEY')) ? true : false;
+ }
 
+ /**
+  * Method verifies are all required/optional elements in POST body request and if yes it sets is into class property.
+  * @param \Illuminate\Http\Request $request
+  * @return bool
+  */
+
+ public function validatePOSTContent($request): bool
+ {
+  switch (false) {
+   case $request->input('apiUrl') && $request->input('apiUrl') !== null:
+    return false;
+   case $request->input('apiName') && $request->input('apiName') !== null:
+    return false;
+  }
+
+  $this->apiUrl  = $request->input('apiUrl');
+  $this->apiName = $request->input('apiName');
+
+  if ($request->input('apiAcceptedDataFreshness') && $request->input('apiAcceptedDataFreshness') !== null && gettype($request->input('apiAcceptedDataFreshness')) === "integer") {
+   $this->apiAcceptedDataFreshness = $request->input('apiAcceptedDataFreshness');
+  }
+
+  return true;
+ }
+
+/**
+ * Method verifies is argument a proper URL string.
+ * @param string $url
+ * @return bool
+ */
+
+ public function validateUrlPOSTContent($url): bool
+ {
+  return $this->testApiUrl($url);
+ }
+
+ /**
+  * Method findes record in DB by column and value arguments.
+  * @param string $column
+  * @param string $value
+  * @return bool
+  */
+
+ public function checkLocalDataExist($column, $value): bool
+ {
+  return $this->repository->findByValue($column, $value) !== null ? true : false;
+ }
+
+
+ /**
+ * Method loads model from DB finded by searching value argument and sets is into class property.
+ * @param string $column
+ * @param string $value
+ * @return bool
+ */
+
+ public function loadFindedDataFromDB($searchingValue): void
+ {
+  $this->dbResponseData = $this->repository->findByValue('api-url', $searchingValue);
+ }
+
+ public function updateFindedDataInDB($sourceUrl, $modelToUpdate): void
+ {
+  $id = $modelToUpdate->value('id');
+  $this->fetchAPI($sourceUrl);
+  $this->repository->update($id, ["api-response" => $this->apiResponseData, "api-name" => $this->apiName]);
+  $data                  = $this->repository->findByValue('id', $id);
+  $this->apiResponseData = $data->value('api-response');
+
+ }
+
+ /**
+  * Method compares timestamps between given as argument Collection and actual timestamp with compareTimestampToNow trait.
+  * @param Collection $localData
+  * @return bool
+  */
+
+ public function verifyLocalDataFreshness($localData): bool
+ {
   $verifyTime = $this->compareTimestampToNow($localData->value('updated_at'), $this->apiAcceptedDataFreshness);
+
   if ($verifyTime) {
-   $this->apiResponseData = $localData->value('api-response');
-
+   return true;
   } else {
-   $this->fetchAPI($this->apiUrl);
-   $id = $localData->value('id');
-   $this->repository->update($id, ["api-response" => $this->apiResponseData, "api-name" => $this->apiName]);
-
+   return false;
   }
  }
+
+ /**
+  * Method fetches data from API and sets it into apiResponseData class property.
+  * @param string $apiUrl
+  * @return void
+  */
 
  public function fetchAPI($apiUrl): void
  {
-  if ($this->testAPIUrl($apiUrl)) {
-   $conn = \curl_init($apiUrl);
-   curl_setopt($conn, CURLOPT_URL, $apiUrl);
-   curl_setopt($conn, CURLOPT_HEADER, false);
-   curl_setopt($conn, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
+  $conn = \curl_init($apiUrl);
 
-   curl_setopt($conn, CURLOPT_RETURNTRANSFER, 1);
-   $data = curl_exec($conn);
+  curl_setopt($conn, CURLOPT_URL, $apiUrl);
+  curl_setopt($conn, CURLOPT_HEADER, false);
+  curl_setopt($conn, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
+  curl_setopt($conn, CURLOPT_RETURNTRANSFER, 1);
 
-   $this->apiResponseData = $data;
-   curl_close($conn);
+  if (!curl_error($conn)) {
+   $this->apiResponseData = curl_exec($conn);
   } else {
-   $this->apiResponseData = '{"connection": "error"}';
-
+   $this->apiResponseData = response('connection error', 500);
   }
+
+  curl_close($conn);
  }
 }
--- routes/api.php
+++ routes/api.php
@@ -20,4 +20,4 @@ use Illuminate\Support\Facades\Route;
 // });
 
 // Route::get('/', [APICorsController::class, 'manageQuery']);
-Route::post('/', [APIController::class, 'apiKeyVerification']);
+Route::post('/', [APIController::class, 'apiController']);
